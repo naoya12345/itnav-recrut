10bbe5fd75f069457fbc243875b1c24c8f31e152
naoya12345 <k1152n0228@gmail.com>
2022-06-02 15:59:53 +0900

    a

--- top/infomation-engineer.js
+++ top/infomation-engineer.js
@@ -17,7 +17,7 @@ const writeHeader = (getData) => {
   };
 };
 
-const headerUrl = "/assets/htmlparts/header.html";
+const headerUrl = "header.html";
 
 fetch(headerUrl)
   .then((response) => response.text())
@@ -42,7 +42,7 @@ const writeFooter = (getData) => {
   }
 };
 
-const footerURL = "/assets/htmlparts/footer.html";
+const footerURL = "footer.html";
 
 fetch(footerURL)
   .then((response) => response.text())
